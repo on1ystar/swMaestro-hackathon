e054321df356bd93392ee91c721be869422aea18
on1ystar <tjdwls0607@naver.com>
2021-05-12 18:14:18 +0900

    src folder update

--- routers/globalRouter.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { home } from "../controllers/userController";
-
-const globalRouter = express.Router();
-
-globalRouter.get("/", home);
-
-export default globalRouter;
